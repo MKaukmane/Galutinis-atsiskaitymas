282b2f35934de0af7b12b4a585c8b10bfff93a69
MKaukmane <baranauskaite.milda@gmail.com>
2024-03-22 13:55:42 +0200

    pirmas planas header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/UI/Header';
+import Home from './components/pages/Home';
+import Register from './components/pages/Register';
+import Login from './components/pages/Login';
 
 const App = () => {
 
   return (
     <>
+      <Header />
+      <main>
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path='/'>
+
+          </Route>
+          <Route path='/user'>
+            <Route path='register' element={<Register />} />
+            <Route path='login' element={<Login />} />
+          </Route>
+        </Routes>
+      </main>
     </>
   );
 }
--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,9 +1,69 @@
+import styled from "styled-components";
+import { Link, NavLink } from "react-router-dom";
 
+const StyledHeader = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 10px;
+
+    font-size: 1.2rem;
+    background-color: #f3f3f3;
+    padding: 0 20px;
+    >div{
+        >a {
+            text-decoration: none;
+            color: black;
+            >img{
+                height: 50px;
+            }
+        }
+        >a:hover{
+            text-decoration: underline;
+            color: #e276fd
+        }
+    }
+    >nav{
+        >ul{
+            display: flex;
+            gap: 10px;
+            list-style-type: none;
+            >li{
+                >a{
+                    text-decoration: none;
+                    color: black;
+
+                }
+                >a:hover{
+                    text-decoration: underline;
+                    color: #e276fd
+                }
+            }
+        }
+    }
+`;
 
 const Header = () => {
     return ( 
-        <>
-        </>
+        <StyledHeader>
+            <div>
+                <Link to="/">
+                    <img src="https://iconape.com/wp-content/files/rl/183006/svg/183006.svg" alt="forum logo"/>
+                </Link>
+            </div>
+            <div><NavLink to='/'>Home</NavLink></div>
+            <div><NavLink to='/addNew'>New question</NavLink></div>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to='/user/login'>Login</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/user/register'>Register</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </StyledHeader>
      );
 }
  
